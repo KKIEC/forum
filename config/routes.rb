8d24315ae97a8d7bba271baf11dcdd9f27dbbc05
Krzysztof Kiec <kkiec95@gmail.com>
2021-11-11 13:50:38 +0100

    CRUD for roles

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'welcome/index'
+
+
+  resources :roles
+  resources :users
+  resources :topics
+  resources :categories
+  resources :posts
+
+  root to: 'welcome#index'
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+r1 = Role.create({ name: 'Admin', description: 'Can do everything' })
+r2 = Role.create({ name: 'Member', description: 'Can do all on things created by own' })
+r3 = Role.create({ name: 'User', description: 'Can perform CRUD on posts' })
