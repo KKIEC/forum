3ba3e9b6f9e606f0c0e244a570b145830c8a8b1a
Krzysztof Kiec <kkiec95@gmail.com>
2021-12-17 15:13:52 +0100

    bootstrap not working on heroku attempt 2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,12 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "bootstrap";
-import "../stylesheets/application"
+
+require("@popperjs/core")
+
+import "bootstrap"
+
+require("../stylesheets/application.scss")
 
 Rails.start()
 Turbolinks.start()
