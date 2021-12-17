c5566eea4d675f02b9864f1c466a422c158d64b7
Krzysztof Kiec <kkiec95@gmail.com>
2021-12-17 18:08:46 +0100

    sole admin protection: validation upgrade

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,10 @@ class User < ApplicationRecord
   validate :at_least_one_admin, on: :update
 
   def at_least_one_admin
-    return unless (User.admin.count <= 1) && ((role == 'member') || (role == 'novice'))
+    user = User.find(id)
+    cond1 = User.admin.count <= 1
+    cond2 = user.role == 'admin'
+    return unless cond1 && cond2 && ((role == 'member') || (role == 'novice'))
 
     errors.add :base, :invalid, message: 'You are the last admin!!!'
   end
